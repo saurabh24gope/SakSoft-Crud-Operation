46bad2b8d73785c111e949036deef3b5e470b43a
saurabh24gope <saurabhgope@gmail.com>
2023-02-26 11:18:37 +0530

    change package jar to war

--- src/main/java/com/shaksoft/crudoperation/CrudOperationApplication.java
+++ src/main/java/com/shaksoft/crudoperation/CrudOperationApplication.java
@@ -4,9 +4,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.SpringServletContainerInitializer;
 
 @SpringBootApplication
-public class CrudOperationApplication {
+public class CrudOperationApplication extends SpringServletContainerInitializer {
 
 	public static void main(String[] args) {
 
